77f1d5613904d98464c7ba68d4a21118b0c0ff70
tonis <tonisives@gmail.com>
2021-06-26 10:24:22 +0700

    Initial Commit

---
+++ ti-kmp-json-macos/ti_kmp_json_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ti_kmp_json_macosApp.swift
+//  ti-kmp-json-macos
+//
+//  Created by Tõnis Tiganik on 26/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ti_kmp_json_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
